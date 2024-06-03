29fd4d39440a3ad27760a1137c95662d8641d0b7
harsh <hasrh946643@gmail.com>
2024-06-03 14:21:01 +0530

    working on body

--- src/components/headers/navigation/index.js
+++ src/components/headers/navigation/index.js
@@ -4,10 +4,10 @@ import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa6";
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
-
+import { FaAngleRight } from "react-icons/fa6";
 const Navigations = () => {
 
-    const [isOpenNav , setIsOpenNav] = useState(false);
+    const [isOpenNav, setIsOpenNav] = useState(false);
 
     return (
         <>
@@ -16,15 +16,39 @@ const Navigations = () => {
                     <div className="row">
                         <div className="col-sm-3 navpart1 d-flex">
                             <div className='catWrapper'>
-                                <Button className="allcat" onClick={()=>setIsOpenNav(!isOpenNav)}>
+                                <Button className="allcat" onClick={() => setIsOpenNav(!isOpenNav)}>
                                     <span className='icon1 mr-2'><IoIosMenu /></span>
                                     <span className="texts">All Catogries</span>
                                     <span className='icon2 ml-2'><FaAngleDown /></span>
                                 </Button>
-                                <div className={`sidebarNav shadow ${isOpenNav==true ? 'open' : ''}`} >
+                                <div className={`sidebarNav shadow ${isOpenNav == true ? 'open' : ''}`} >
                                     <ul>
-                                        <li><Link to='/men'><Button>Home</Button></Link></li>
-                                        <li><Link to='/men'><Button>Men</Button></Link></li>
+                                        <li><Link to='/'><Button>Home <FaAngleRight className='ml-auto' /></Button></Link>
+                                            <div className='submenu'>
+                                                <Link to='/men'><Button>Clothing</Button></Link>
+                                                <Link to='/men'><Button>Footwear</Button></Link>
+                                                <Link to='/men'><Button>watches</Button></Link>
+                                                <Link to='/men'><Button>Clothing</Button></Link>
+                                                <Link to='/men'><Button>Footwear</Button></Link>
+                                                <Link to='/men'><Button>watches</Button></Link>
+                                                <Link to='/men'><Button>Clothing</Button></Link>
+                                                <Link to='/men'><Button>Footwear</Button></Link>
+                                                <Link to='/men'><Button>watches</Button></Link>
+                                            </div>
+                                        </li>
+                                        <li><Link to='/men'><Button>Men <FaAngleRight className='ml-auto' /></Button></Link>
+                                            <div className='submenu'>
+                                                <Link to='/men'><Button>Clothing</Button></Link>
+                                                <Link to='/men'><Button>Footwear</Button></Link>
+                                                <Link to='/men'><Button>watches</Button></Link>
+                                                <Link to='/men'><Button>Clothing</Button></Link>
+                                                <Link to='/men'><Button>Footwear</Button></Link>
+                                                <Link to='/men'><Button>watches</Button></Link>
+                                                <Link to='/men'><Button>Clothing</Button></Link>
+                                                <Link to='/men'><Button>Footwear</Button></Link>
+                                                <Link to='/men'><Button>watches</Button></Link>
+                                            </div>
+                                        </li>
                                         <li><Link to='/men'><Button>Women</Button></Link></li>
                                         <li><Link to='/men'><Button>Kids</Button></Link></li>
                                         <li><Link to='/men'><Button>Watches</Button></Link></li>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,6 +9,25 @@ const Home = () => {
     return (
         <>
             <HomeBanner/>
+            <div className="homeProducts">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <div className="banner">
+                                <img className="cursor" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg" alt=""></img>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="d-flex align-items-center">
+                                <div className="info">
+                                    <h3>Best Seller</h3>
+                                    <p>Do not miss the current offers until the end of the march</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
