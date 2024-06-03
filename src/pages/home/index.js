2dc86377d89c5bb7d2633014acdd795d1c390573
harsh <hasrh946643@gmail.com>
2024-06-03 23:07:36 +0530

    working on body

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,22 @@
 import HomeBanner from "../../components/homebanner";
-
-
+import Button from '@mui/material/Button';
+import { FaArrowRight } from "react-icons/fa";
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation } from 'swiper/modules';
 
 const Home = () => {
-    
-    
-
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    };
     return (
         <>
-            <HomeBanner/>
+            <HomeBanner />
             <div className="homeProducts">
                 <div className="container">
                     <div className="row">
@@ -17,12 +25,74 @@ const Home = () => {
                                 <img className="cursor" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg" alt=""></img>
                             </div>
                         </div>
-                        <div className="col-md-3">
-                            <div className="d-flex align-items-center">
+                        <div className="col-md-9 productRow">
+                            <div className="d-flex align-items-center ">
                                 <div className="info">
-                                    <h3>Best Seller</h3>
-                                    <p>Do not miss the current offers until the end of the march</p>
+                                    <h3 className="mb-0 hd">Best Seller</h3>
+                                    <p className="text-light">Do not miss the current offers until the end of the march</p>
                                 </div>
+                                <Button className="viewButton ml-auto">View all <FaArrowRight className="ml-auto" /></Button>
+                            </div>
+                            <div className="product_row">
+                                <Swiper
+                                    slidesPerView={4}
+                                    spaceBetween={30}
+                                    pagination={{
+                                        clickable: true,
+                                    }}
+                                    modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="productItems d-flex">
+                                            <div className="img_wrapper">
+                                                <img className="w-100" src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt=""></img>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
                             </div>
                         </div>
                     </div>
